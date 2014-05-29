1a6de84263ad779202b0f921231ef0949df157d8
James Taylor <taylorjames9@gmail.com>
2014-05-29 15:06:00 -0500

    Text Input Field now working in Puzzle1 scene

---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/ScriptsForPuzzles/TextInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextInput : MonoBehaviour {
+
+	public string stringToEdit = "Insert Code";
+
+	public GUIStyle inputTextStyle;
+
+	// Use this for initialization
+	void Start () {
+
+		///////inputTextStyle = new GUIStyle();
+		//inputTextStyle.fontSize = 12;
+
+	}
+
+	void OnGUI() {
+		stringToEdit = GUI.TextField(new Rect(300, 300, 165,35), stringToEdit, 10, inputTextStyle);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
