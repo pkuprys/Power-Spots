935a546db5056d8422b0d7791abd3909aac58db5
Petras Kuprys <pkuprys@gmail.com>
2014-06-02 22:24:59 -0500

    Clicking a spot will open the first puzzle now

---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/PuzzleLauncher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleLauncher : MonoBehaviour {
+	void Start () {}
+	void Update () {}
+
+    public void OnMouseOver(){
+        if(Input.GetMouseButtonDown(0)){
+            Application.LoadLevel("PS_Puzzle1");
+        }
+    }
+}
