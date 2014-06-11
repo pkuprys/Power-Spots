4f94dc8d10cdfa5a23dcc41374a4c77ece0721f6
James Taylor <taylorjames9@gmail.com>
2014-06-11 11:24:31 -0500

    Adding a few backups to backupTwo

---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/Tray.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tray : MonoBehaviour {
+
+
+		public Texture trayTexture;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+		void OnGUI(){
+
+				GUI.DrawTexture (new Rect (Screen.width / 4, Screen.height - 98, Screen.width / 2, 98), trayTexture);
+
+		}
+
+}
