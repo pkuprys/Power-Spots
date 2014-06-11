6d2e04c2918e89e6b4195195d1144a4b86b4992e
Petras Kuprys <pkuprys@gmail.com>
2014-06-10 22:08:55 -0500

    collision detection for spots is now working did some work (more like research) on getting the animation to display on a spot when the player is nearby

--- MapNav_3_PowerS_5_22/Assets/Scripts/PuzzleLauncher.cs
+++ MapNav_3_PowerS_5_22/Assets/Scripts/PuzzleLauncher.cs
@@ -3,11 +3,25 @@ using System.Collections;
 
 public class PuzzleLauncher : MonoBehaviour {
 	void Start () {}
-	void Update () {}
+	void Update () {
+        if(isActive){
+            //play animation?
+        }
+    }
+
+    private bool isActive = false;
 
     public void OnMouseOver(){
         if(Input.GetMouseButtonDown(0)){
              StartCoroutine(GameManager.Instance.Challenge(this.gameObject.name));
         }
+    }
+
+    public void OnTriggerEnter (Collider col){
+        isActive = true;
+    }
+
+    public void OnTriggerExit (Collider col){
+        isActive = false;
     }
 }
