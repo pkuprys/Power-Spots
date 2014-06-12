71aee7e3f01fcc42293978fbd35f09afb8f7fbfd
Petras Kuprys <pkuprys@gmail.com>
2014-06-11 21:58:37 -0500

    Animations show up on spots when collision is detected Puzzles get launched when clicked on

--- MapNav_3_PowerS_5_22/Assets/Scripts/PuzzleLauncher.cs
+++ MapNav_3_PowerS_5_22/Assets/Scripts/PuzzleLauncher.cs
@@ -2,26 +2,30 @@
 using System.Collections;
 
 public class PuzzleLauncher : MonoBehaviour {
-	void Start () {}
-	void Update () {
-        if(isActive){
-            //play animation?
-        }
-    }
-
+    private GameObject redCircle;
     private bool isActive = false;
 
+    void Start () {
+        redCircle = GameObject.Find(this.gameObject.name + "_red_circle");
+        redCircle.active = isActive;
+    }
+    void Update () {}
+
+
     public void OnMouseOver(){
-        if(Input.GetMouseButtonDown(0)){
+        if(Input.GetMouseButtonDown(0) && isActive){
+            print("STARTING");
              StartCoroutine(GameManager.Instance.Challenge(this.gameObject.name));
         }
     }
 
     public void OnTriggerEnter (Collider col){
         isActive = true;
+        redCircle.SetActive(true);
     }
 
     public void OnTriggerExit (Collider col){
         isActive = false;
+        redCircle.SetActive(false);
     }
 }
