a913a46d872eb5397028301dc2088f8b043bd85c
Petras Kuprys <pkuprys@gmail.com>
2014-06-12 21:35:28 -0500

    added text field and buttons to puzzle 2

--- MapNav_3_PowerS_5_22/Assets/Scripts/ScriptsForPuzzles/TextInput.cs
+++ MapNav_3_PowerS_5_22/Assets/Scripts/ScriptsForPuzzles/TextInput.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class TextInput : MonoBehaviour {
 	public string StringToEdit = "Insert Code";
 	public GUIStyle inputTextStyle;
+    public float xPos;
+    public float yPos;
+    public float width;
+    public float height;
 
 	void Start () {}
     void Update () {}
 
 	void OnGUI() {
-		StringToEdit = GUI.TextField(new Rect(750, 650, 200, 35), StringToEdit, 100, inputTextStyle);
+		StringToEdit = GUI.TextField(new Rect(xPos, yPos, width, height), StringToEdit, 100, inputTextStyle);
 	}
 }
