39b77453f42cc036574c0cb2e0a24d1358c2ca18
James Taylor <taylorjames9@gmail.com>
2014-06-17 18:05:13 -0500

    Added a placeholder for the team color in bottom right of map scene

---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/TeamColorReminder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeamColorReminder : MonoBehaviour {
+
+
+	public Texture teamColorReminder;
+
+
+	void OnGUI(){
+
+		if (!teamColorReminder) {
+			Debug.LogError("Assign a Texture in the inspector.");
+			return;
+		}
+
+
+		GUI.DrawTexture(new Rect(Screen.width-50, Screen.height -50, 40, 40), teamColorReminder);
+
+	}
+}
--- MapNav_3_PowerS_5_22/Assets/Scripts/Tray.cs
+++ MapNav_3_PowerS_5_22/Assets/Scripts/Tray.cs
@@ -18,18 +18,13 @@ public class Tray : MonoBehaviour {
 	void Start () {
 
 		//mapnavScript = mapNav.GetComponent(MapNav);
-
 		duration = 0.1f;
 		startY = Screen.height - 25;
 		endY = Screen.height-118;
 		startTime = Time.time;
 		toggleImg = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 		void OnGUI(){
 
 		if (!trayTexture) {
@@ -47,5 +42,6 @@ public class Tray : MonoBehaviour {
 		}
 		//if(mapnav.ready)
 		toggleImg = GUI.Toggle (new Rect (Screen.width / 4, yPos, Screen.width / 2, 118), toggleImg, trayTexture);
+
 	}
 }
