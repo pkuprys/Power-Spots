b5ffdbb5c054b46be93e64efd50985e9a3cf52f8
James Taylor <taylorjames9@gmail.com>
2014-06-18 18:34:33 -0500

    Puzzle_2 now accepts three textInput Responses to light up the circle

---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/GlowingCircleScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowingCircleScript : MonoBehaviour {
+
+	public Texture yellowCircletexture;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (PuzzleSubmitButton_2.correctAnswers >= 3)
+			renderer.material.mainTexture = yellowCircletexture;
+
+	}
+}
---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/PuzzleSubmitButton_2.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleSubmitButton_2 : MonoBehaviour {
+
+	public string answer1 = "123456789";
+
+	public Color c1 = Color.red;
+	public Color c2 = Color.yellow;
+	public Color c3 = Color.blue;
+
+	public GameObject lineRendererObject_1;
+
+	public GameObject myTextInput;
+
+	public static int correctAnswers = 0;
+
+	public Material red;
+	public Material yellow;
+	public Material blue;
+
+	void Start () {
+
+		lineRendererObject_1.renderer.material = red;
+
+	}
+	void Update () {}
+
+	public void OnMouseOver(){
+		if(Input.GetMouseButtonUp(0)){
+			if(myTextInput.GetComponent<TextInput>().StringToEdit.Equals(answer1)){
+
+				lineRendererObject_1.renderer.material = yellow;
+				correctAnswers++;
+				print ("Correct Answers : " + correctAnswers);
+
+			}
+		
+		}
+	}
+}
