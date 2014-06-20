06cbbe2d174fcde14f914d335db6f4f103c87ed6
James Taylor <taylorjames9@gmail.com>
2014-06-20 12:55:12 -0500

    added a button on map screen to view Endcanvas area

---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/GoToEndCanvasBTN.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToEndCanvasBTN : MonoBehaviour {
+
+	public Texture goToEndCanvas;
+
+	public float xPos;
+	public float yPos;
+	public float width;
+	public float height;
+
+
+
+	void OnGUI(){
+
+		if (!goToEndCanvas) {
+			Debug.LogError("Assign a Texture in the inspector.");
+			return;
+		}
+
+
+		if (GUI.Button (new Rect (Screen.width - 200, Screen.height - 50, 40, 40), goToEndCanvas)) {
+
+			Application.LoadLevel ("PS_EndCanvasArea_Scene");
+		}
+
+	}
+}
