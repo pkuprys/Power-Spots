5110b46f3e996a223e85a08eca250477d1000ee2
James Taylor <taylorjames9@gmail.com>
2014-06-26 17:54:11 -0500

    added a new side Tray in Map Scene

---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/Tray_Side.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tray_Side : MonoBehaviour {
+
+
+		public Texture trayGraphic; 
+		public Texture trayGraphicDIM; 
+
+		public Texture myTeamToken;
+
+		void OnGUI(){
+
+				GUI.DrawTexture(new Rect(Screen.width-220, 10, 200, 100), trayGraphic);
+				GUI.DrawTexture(new Rect(Screen.width-110, 55, 20, 20), myTeamToken);
+		}
+}
