e649c52fbe2db92bcaf0cdc29a559caf70b2aa96
Petras Kuprys <pkuprys@gmail.com>
2014-06-25 20:34:07 -0500

    Added loading of currently logged in team's color reminder in map scene

--- MapNav_3_PowerS_5_22/Assets/Scripts/TeamColorReminder.cs
+++ MapNav_3_PowerS_5_22/Assets/Scripts/TeamColorReminder.cs
@@ -2,20 +2,19 @@
 using System.Collections;
 
 public class TeamColorReminder : MonoBehaviour {
-
-
+    private static string REMINDER_PREFIX = "MyTeamIs";
 	public Texture teamColorReminder;
 
+    public void Start(){
+        Team team = LoginManager.Instance.GetSelectedTeam();
+        teamColorReminder = Resources.Load("MyTeamColorGraphic/" + REMINDER_PREFIX + team.Name, typeof(Texture)) as Texture;
+    }
 
-	void OnGUI(){
-
+	public void OnGUI(){
 		if (!teamColorReminder) {
 			Debug.LogError("Assign a Texture in the inspector.");
 			return;
 		}
-
-
-		GUI.DrawTexture(new Rect(Screen.width-50, Screen.height -50, 40, 40), teamColorReminder);
-
+		GUI.DrawTexture(new Rect(Screen.width-100, Screen.height-50, 100, 50), teamColorReminder);
 	}
 }
