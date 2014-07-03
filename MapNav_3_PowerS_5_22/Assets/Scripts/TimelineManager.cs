a52714118358d546dbbb7bea4ca31e7b58da3c60
Petras Kuprys <pkuprys@gmail.com>
2014-07-02 20:59:14 -0500

    Added endCanvas script to display cards

---
+++ MapNav_3_PowerS_5_22/Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,23 @@
+﻿using Parse;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TimelineManager : MonoBehaviour {
+    private static string EVENT_CARD_MODIFIER = "_MODIFIER";
+	void Start () {
+        StartCoroutine("RenderTimeline");
+	}
+
+    private IEnumerator RenderTimeline(){
+        var query = new ParseQuery<Team>().FindAsync();
+        while(!query.IsCompleted) yield return null;
+        IEnumerable<Team> teams = query.Result;
+        foreach(Team team in teams){
+            GameObject.Find(team.Name + EVENT_CARD_MODIFIER).SetActive(team.IsTextSnippetVisible());
+        }
+    }
+}
