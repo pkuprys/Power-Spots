2a13f6866dc1eed5c4723bd0e10014b430195641
James Taylor <taylorjames9@gmail.com>
2014-07-10 10:17:13 -0500

    Applied Background to all puzzle scenes

---
+++ MapNav_3_PowerS_5_22/Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public double timeOn = 0.1;
+	double timeOff = 0.5;
+	private double changeTime = 0;
+
+	void Update() {
+		if (Time.time > changeTime) {
+			light.enabled = !light.enabled;
+			if (light.enabled) {
+				changeTime = Time.time + timeOn;
+			} else {
+				changeTime = Time.time + timeOff;
+			}
+		}
+	}
+}
