f9270f7a4b37f5081847fd205ed249451caf36dc
James Taylor <taylorjames9@gmail.com>
2014-07-15 15:32:15 -0500

    Fixed CTS

--- MapNav_3_PowerS_5_22/Assets/Scripts/GameConstants.cs
+++ MapNav_3_PowerS_5_22/Assets/Scripts/GameConstants.cs
@@ -1,5 +1,5 @@
 ﻿public class GameConstants {
-    public static float POLL_INTERVAL = 1f;
+	public static float POLL_INTERVAL = 3.0f;
     public static int TEAM_COUNT = 8;
     public static int DISABLED_LAYER = 9;
     public static int ENABLED_LAYER = 0;
